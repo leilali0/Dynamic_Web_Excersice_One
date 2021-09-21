dc0ec5f31afd243b1cd512a784f57e0e3c39889b
leilali0 <46583372+leilali0@users.noreply.github.com>
2021-09-21 09:33:26 -0400

    Tuesday Sep 21

--- src/containers/Article.js
+++ src/containers/Article.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { useParams } from 'react-router';
+import Data from '../components/data.json';
 
 function Article() {
-	const articleTitle = 'Single Articles';
-	return (
-		<div>
-			<h1>{articleTitle}</h1>
-		</div>
+	const [article, setArticle] = useState({});
+
+	let { id } = useParams();
+
+	useEffect(() => {
+		const dataToSet = Data.find((item) => item.id === id);
+		setArticle(dataToSet);
+	}, [id]); //the function will run whenever the id changes 
+
+return (
+		<main>
+			<section className="ArticleHeader">
+				<div className="ArticleHeaderText">
+					<h1 className="HeaderOneStyle">{article.title}</h1>
+					<p className="ArticleHeaderDate">{article.publishedDate}</p>
+					<p className="ArticleHeaderBlurb">{article.blurb}</p>
+				</div>
+			</section>
+			<section className="ArticleText">
+				{article.articleText && 
+					article.articleText.map((text, i) => {
+						return <p key={i}>{text.data}</p>;
+				})}
+			</section>
+		</main>
 	);
 }
 
